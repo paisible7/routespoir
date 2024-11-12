7d9a80f7641dcff2c6d54ca06e7ea4e68b52eab3
paisible7 <22km127@esisalama.org>
2024-11-12 17:36:29 +0200

    COMMIT_12_NOVEMBRE_2024 ==> configuration de la modification et suppression des activités

--- lib/services/activite_service.dart
+++ lib/services/activite_service.dart
@@ -9,6 +9,13 @@ class ActivityService {
     _activities.add(activity);
   }
 
+  void updateActivity(Activity updatedActivity) {
+  final index = _activities.indexWhere((activity) => activity.id == updatedActivity.id);
+  if (index != -1) {
+    _activities[index] = updatedActivity;
+  }
+}
+
   void removeActivity(String id) {
     _activities.removeWhere((activity) => activity.id == id);
   }
